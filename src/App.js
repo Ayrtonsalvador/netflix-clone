16e9b30db279bca2ba1f6c991763c4548eabf5cf
Salvador <salvador.ayrton@gmail.com>
2021-11-27 13:03:30 +0100

    added navbar + youtube trailer but ndoesnt work yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="app">   
+
+      <Nav/>
 
       <Banner />
 
@@ -17,7 +20,9 @@ function App() {
 
       <Row title="Trending Now" fetchUrl={requests.fetchTrendingNow}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -4,7 +4,9 @@ const requests = {
     fetchTrendingNow: `/trending/all/day?api_key=${API_KEY}`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=en-US`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchComediesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
 }
 
 export default requests;
